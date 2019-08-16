8b8b1efe6e6ba74b667e9417dd5f85618b4715c1
Unni KS <unni@makeitz.com>
2019-08-16 22:44:47 +0530

    Intial commits

--- resources/views/vendor/KenHyuwa/components/partner.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="partners">
-    <h1>Partners</h1>
-    <ul>
-        <li><a href="https://onphpid.com" target="_blank">onphpid.com</a></li>
-    </ul>
-</div>
-<div class="partners_footer">
-    <h1><a href="mailto:onphpid@gmail.com" target="_top">Become A Partners</a></h1>
-</div>
