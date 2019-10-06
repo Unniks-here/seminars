bcceb38a71eec6d6afe646b09a0efa447da6ce8f
Vinil Vinayan <vinil@makeitz.com>
2019-10-07 02:33:44 +0530

    7-10-19

--- resources/views/pages/speakers.blade.php
+++ resources/views/pages/speakers.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container">
 	<div class="row ">
-        <div class="col-sm-4 "></div>
+        <div class="col-sm-2 "></div>
       <!-- <h1 style="text-align: center">Committee</h1> -->
       <!-- <hr class="divider  my-4">  -->
       <div class="row justify-content-center">
-        <div class="col-sm-8">  
+        <div class="col-sm-12">  
           <div class="row">
               <div class="col-sm-12">
-                  <h4 style="color:#26ad6d; text-align: center">Expected Speakers</h4>
+                  <h4 style="color:#26ad6d; text-align: center">List of Speakers</h4>
               </div>
           </div>
             <div class="row ">
@@ -19,153 +19,81 @@
                       
                             <ul style="list-style: none;">
                               <li><b>Prof. Takeo Yamaguchi</b><br><font size="2">
-                                Professor, Tokyo Institute of Technology<br>
-                                Institute of Innovative Research<br>
-                                Laboratory for Chemistry and Life Science<br>
-                                R1-17, 4259 Nagatsuta, Midori-ku, Yokohama 226-8503, JAPAN<br></font>
+                                Tokyo Institute of Technology, Japan<br></font>
                                 <hr>
                               </li>
-                              <li><b>Dr. Takanori Tamaki</b><br><font size="2">
-                                Associate Professor<br>
-                                Tokyo Institute of Technology<br>
-                                Institute of Innovative Research<br>
-                                Laboratory for Chemistry and Life Science<br>
-                                R1-17, 4259 Nagatsuta, Midori-ku, Yokohama 226-8503, JAPAN<br></font><hr>
+                              <li><b>Dr. Gopinathan M. Anilkumar</b><br><font size="2"> 
+                                Noritake, Japan<br></font><hr>
                               </li>
-                              <li><b>Hideto Imai, Ph. D. </b><br><font size="2">
-                                General Manager<br>
-                                Analysis Platform Department<br>
-                                NISSAN ARC Ltd.,<br>
-                                1, Natsushima, Yokosuka, Japan, 237-0061<br></font><hr>
+                              <li><b>Dr. Takanori Tamaki</b><br><font size="2">
+                                Tokyo Institute of Technology, Japan<br></font><hr>
+                              </li>
+                              <li><b>Prof. Suresh C Pillai</b><br><font size="2">
+                                Institute of Technology, Sligo, Ireland<br></font><hr>
+                              </li>
+                              <li><b>Dr. Sindhuja Sankaran</b><br> <font size="2">
+                                Washington State University, USA<br> </font><hr>
                               </li>
                               <li><b>Prof. Rajan Jose</b><br><font size="2">
-                                Dean of Research (Technology) <br>
-                                Research & Innovation Centre<br>
-                                Faculty of Industrial Sciences & Technology <br>
-                                Universiti Malaysia Pahang <br>
-                                Lebuhraya Tun Razak, Gambang 26300 <br>
-                                Kuantan, Malaysia.<br></font><hr>
+                                Universiti Malaysia Pahang, Malaysia<br> </font><hr>
                               </li>
-                              <li><b>Prof. Paula M Vilarinho</b><br> <font size="2">
-                                Professor<br> 
-                                Department of Materials and Ceramic Engineering <br> 
-                                University of Aveiro<br> 
-                                Portugal<br> </font><hr>
+                              <li><b>Dr. KGK Warrier</b><br> <font size="2">
+                                CSIR-NIIST, Thiruvananthapuram, India<br> </font><hr>
                               </li>
-                              <li><b>Prof. Heli Jantunen </b><br><font size="2">
-                                Professor & Head of Electronic Materials <br> 
-                                Microelectronics Research Unit <br> 
-                                Faculty of Information Technology and Electrical Engineering <br> 
-                                P. O. BOX 4500, FI-90014 <br> 
-                                University of Oulu Finland <br> </font><hr>
-                              </li>
-                              <li><b>Dr. Gopinathan M. Anil kumar</b><br> <font size="2">
-                                DGM, R&D Center<br> 
-                                Noritake Co., Ltd<br> 
-                                300 Higashiyama, Miyoshicho, Miyoshi<br> 
-                                Aichi Pref. Japan<br> </font><hr>
-                              </li>
-                              <li><b>Prof. Suresh C. Pillai, PhD, MBA, FIMMM, FRMS</b><br><font size="2">
-                                Centre for Precision Engineering, Materials and Manufacturing Research &<br>
-                                Nanotechnology and Bio-Engineering Research Division, Department of   Environmental Science,<br>
-                                Institute of Technology Sligo,<br>
-                                Ash Lane, Sligo, Ireland<br></font><hr>
-                              </li>
-                              <li><b>Dr. Lav R. Khot</b><br><font size="2">
-                                Assistant Professor<br>
-                                Department of Biological System Engineering <br>
-                                Washington State University, USA<br>
-                              </li></font><hr>
+                              <li><b>Dr. Sujatha Devi</b><br><font size="2">
+                                CSIR-NIIST, Thiruvananthapuram, India<br> </font><hr>
+                              </li> 
+                              <li><b>Dr. Paresh L. Dhepe</b><br><font size="2">
+                                NCL, Pune, India<br> </font><hr>
+                              </li> 
+                              <li><b>Dr. Mahesh Hariharan</b><br><font size="2">
+                                IISER, Thiruvananthapuram, India<br> </font><hr>
+                              </li> 
                             </ul>
                     </div>
                     <div class="col-sm-6 ">
                         <hr class="divider light my-4">
                           
                                 <ul style="list-style: none;">
-                                    <li><b>Dr. Mahesh Hariharan</b><br><font size="2">
-                                      Associate Professor<br>
-                                      Department of Chemistry<br>
-                                      Indian Institute of Science Education and Research (IISER)<br>
-                                      Thiruvananthapuram<br></font><hr>
+                                    <li><b>Dr. Sudarshan Kurmar</b><br><font size="2">
+                                      K, IIT Guwahati, India<br></font><hr>
                                     </li>
-                                    <li><b>Dr. Shajesh P. </b><br><font size="2">
-                                      Associate Professor <br>
-                                      CNMS Jain Global Campus <br>
-                                      Jakkasandra Post <br>
-                                      Bangalore. <br>
-                                      Pin 562112 <br></font><hr>
+                                    <li><b>Dr. Shajesh P</b><br><font size="2">
+                                      Jain University, Bangalore, India<br></font><hr>
                                     </li>
-                                  <li><b>Dr. Sudarshan Kumar K.</b><br><font size="2">
-                                      Assistant Professor<br>
-                                      Department of Mathematics<br>
-                                      Indian Institute of Technology Guwahati<br></font><hr>
+                                  <li><b>Dr. Nandini Devi. R</b><br><font size="2">
+                                    NCL, Pune, India<br></font><hr>
                                     
                                   </li>
-                                  <li><b>Dr. Sindhuja Sankaran</b><br><font size="2">
-                                      Associate Professor<br>
-                                      Biological Systems Engineering<br>
-                                      Washington State University<br>
-                                      Pullman, WA 99164-6120<br></font><hr>
+                                  <li><b>Dr. Suraj Somon</b><br><font size="2">
+                                    CSIR-NIIST, Thiruvananthapuram, India<br></font><hr>
                                     
-                                  </li>
-                                  <li><b>Dr. Pramod Pillai </b><br><font size="2">
-                                      Assistant Professor<br>
-                                      Department of Chemistry<br>
-                                      IISER, Pune<br></font><hr>
-                                    
-                                  </li>
-                                  <li><b>Dr. Sujatha Devi. P </b><br><font size="2">
-                                      Senior Principal Scientist and Head, CSTD <br>
-                                      NIIST, Thiruvananthapuram, India<br></font><hr>
-                                    
-                                  </li>
-                                  <li><b>Dr. KGK Warrier </b><br><font size="2">
-                                      Rtd. Senior Principal Scientist<br>
-                                      Chief Ceramic Consultant <br>
-                                      CSIR-NIIST<br>
-                                      Thiruvananthapuram<br></font><hr>
-                                     
-                                  </li>
-                                  <li><b>Dr. Suraj Soman</b><br><font size="2">
-                                      Scientist<br>
-                                      CSIR-NIIST <br>
-                                      Thiruvananthapuram<br></font><hr>
-                                     
                                   </li>
                                   <li><b>Dr. Bibin John</b><br><font size="2">
-                                      Scientist<br>
-                                      Energy Systems Division <br>
-                                      Vikram Sarabhai Space Centre (VSSC) <br>
-                                      Thiruvananthapuram, Kerala, India<br></font><hr>
+                                    VSSC, Thiruvananthapuram, India<br></font><hr>
                                     
                                   </li>
-                                  <li><b>Dr. Baiju Vijayan</b><br><font size="2">
-                                      Assistant Professor & HoD <br>
-                                      Department of Nanoscience <br>
-                                      Kannur University<br></font><hr>
+                                  <li><b>Dr. Rajesh Parishwad</b><br><font size="2">
+                                    Royal Society of Chemistry, India<br></font><hr>
                                     
                                   </li>
-                                  <li><b>Dr. Manfredi Saeli</b><br><font size="2">
-                                      Dipertimento di Architettura<br>
-                                      Università degli Studi di Palermo<br>
-                                      Piazza Marina, 61<br>
-                                      90133 Palermo<br>
-                                      Italy<br></font><hr>
+                                  <li><b>Prof. Youssef Belmabkhout </b><br><font size="2">
+                                    Mohamed VI Polytechnic University, Morocco<br></font><hr>
+                                     
+                                  </li>
+                                  <li><b>Dr. Praveen Sarojam</b><br><font size="2">
+                                    Gulf Bio Analytical LLC, UAE<br></font><hr>
+                                     
+                                  </li>
+                                  <li><b>Prof. Mohd Hasbi Ab Rahim</b><br><font size="2">
+                                    Universiti Malaysia Pahang, Malaysia<br></font><hr>
                                     
                                   </li>
-                                  <li><b>Dr. Dalia Vishnudasan</b><br><font size="2">
-                                      Asst Professor<br>
-                                      School of Biotechnology<br>
-                                      Amrita Vishwa Vidyapeedam<br>
-                                      Amrta Puri Campus, Kollam.<br></font><hr>
+                                  <li><b>Dr. Lav R. Khot</b><br><font size="2">
+                                    Washington State University, USA<br></font><hr>
                                     
                                   </li>
-                                  <li><b>Dr. Prakash P Neelakandan </b><br><font size="2">
-                                      Scientist E<br>
-                                      Institute of Nano Science and Technology (INST) <br>
-                                      Mohali<br></font><hr>
-                                    
-                                  </li>
+                                     
                                 </ol>
                         </div>
                   </div>
